c581a952504bc2935845dfa9651072bf6b379344
hrdksny <hardiksoni1987@hotmail.com>
2018-03-01 18:39:32 +0530

    Added SPI Support

--- src/main.c
+++ src/main.c
@@ -12,6 +12,7 @@
 #include <stm32f10x.h>
 #include <stm32f10x_gpio.h>
 #include <stm32f10x_rcc.h>
+#include <stm32f10x_spi.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <tim2_delay_custom.h>
@@ -295,9 +296,29 @@ void SetSysClockTo72(void)
         }
     }
 }
-
 /*---------------------------------------- USB Related END---------------------------------------------------*/
 
+void _sendData(uint8_t c)
+{
+	//while(SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET)
+	{
+	}
+	SPI_I2S_SendData(SPI1, c);
+	while(SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_BSY) == SET)
+	{
+	}
+}
+void _readData(uint8_t Add, uint8_t* Data)
+{
+	SPI_NSSInternalSoftwareConfig(SPI1, SPI_NSSInternalSoft_Reset);
+	_sendData(Add);
+
+	//while(SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE) == RESET)
+	{
+	}
+	*Data = SPI_I2S_ReceiveData(SPI1);
+	SPI_NSSInternalSoftwareConfig(SPI1, SPI_NSSInternalSoft_Set);
+}
 
 /*	GPIO Toggle*/
 int main(void)
@@ -307,10 +328,11 @@ printf("Starting....!!\r\n");
 
 char buffer[80] = {'\0'};
 uint32_t RTC_Counter = 0;
-int i;
+uint8_t i;
 
 RTC_DateTimeTypeDef	RTC_DateTime;
 GPIO_InitTypeDef  GPIO_InitStructure;
+SPI_InitTypeDef SPI_InitStructure;
 
 SetSysClockToHSE();
 TIM2_init();
@@ -332,6 +354,34 @@ GPIO_Init(GPIOC, &GPIO_InitStructure);
 
 //GPIO_SetBits(GPIOC, GPIO_Pin_13); // Set C13 to High level ("1")
 GPIO_ResetBits(GPIOC, GPIO_Pin_13); // Set C13 to Low level ("0")
+
+RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7/*MOSI1*/ |  GPIO_Pin_5;/*SCLK*/
+GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_4;/*MISO1*/
+GPIO_Init(GPIOA, &GPIO_InitStructure);
+//GPIO_ResetBits(GPIOA, GPIO_Pin_6);
+//GPIO_SetBits(GPIOA, GPIO_Pin_6);
+
+SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
+//SPI_InitStructure.SPI_Direction = SPI_Direction_1Line_Tx;
+SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
+SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
+SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
+SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
+SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_8; /*(Fpclk/8 = 8Mhz/8 = 1Mhz)*/
+SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
+SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
+SPI_Init(SPI1, &SPI_InitStructure);
+SPI_Cmd(SPI1, ENABLE);
+SPI_NSSInternalSoftwareConfig(SPI1, SPI_NSSInternalSoft_Set);
 
 if (RTC_Init() == 1)
 {
@@ -366,9 +416,11 @@ while(1)
 	while (RTC_Counter == RTC_GetCounter())
 	{
 	}
+	_readData(0x75,&i);
+	printf("I_am = %d\n\r",i);
 }
 
-while (1)
+/*while (1)
 {
 	if (bDeviceState == CONFIGURED)
 	{
@@ -399,19 +451,10 @@ while (1)
 
 while (1)
 {
-	/* Toggle LED which connected to PC13*/
+	 Toggle LED which connected to PC13
 	GPIOC->ODR ^= GPIO_Pin_13; // Invert C13
-	/* delay */
+	 delay
 	for(i=0;i<0x100000;i++);
 	printf("Starting !!..\n\r");
-}
-}
-
-/*int main(void)
-{
-
-	for(;;)
-	{
-		printf("Hello World");
-	}
 }*/
+}
