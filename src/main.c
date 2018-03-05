ee2d43710f250cf66b8731c89fecf67d6d9e5057
hrdksny <hardiksoni1987@hotmail.com>
2018-03-05 19:47:57 +0530

    SPI Testing

--- src/main.c
+++ src/main.c
@@ -22,6 +22,7 @@
 #include "usb_desc.h"
 #include "usb_pwr.h"
 #include "usb_init.h"
+#include "MPU9250.h"
 
 extern volatile uint8_t Receive_Buffer[64];
 extern volatile uint32_t Receive_length ;
@@ -300,25 +301,125 @@ void SetSysClockTo72(void)
 
 void _sendData(uint8_t c)
 {
-	//while(SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET)
-	{
-	}
+	//GPIOC->ODR &= ~GPIO_Pin_4; // ReSet Pin CS//GPIO_ResetBits(GPIOA, GPIO_Pin_4);
+//	while(SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET)
+//	{
+//	}
 	SPI_I2S_SendData(SPI1, c);
 	while(SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_BSY) == SET)
 	{
 	}
+	//GPIOC->ODR |= GPIO_Pin_4; // Set Pin CS//GPIO_SetBits(GPIOA, GPIO_Pin_4);
 }
-void _readData(uint8_t Add, uint8_t* Data)
+uint16_t _readData(uint16_t* Data)
 {
-	SPI_NSSInternalSoftwareConfig(SPI1, SPI_NSSInternalSoft_Reset);
-	_sendData(Add);
-
-	//while(SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE) == RESET)
+	uint16_t Data1 = 0;
+	//GPIOC->ODR &= ~GPIO_Pin_4; // ReSet Pin CS//GPIO_ResetBits(GPIOA, GPIO_Pin_4);
+	//SPI_NSSInternalSoftwareConfig(SPI1, SPI_NSSInternalSoft_Reset);
+	while(SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE) == RESET)
 	{
 	}
-	*Data = SPI_I2S_ReceiveData(SPI1);
-	SPI_NSSInternalSoftwareConfig(SPI1, SPI_NSSInternalSoft_Set);
+	Data1 = SPI_I2S_ReceiveData(SPI1);
+	//GPIOC->ODR |= GPIO_Pin_4; // Set Pin CS//GPIO_SetBits(GPIOA, GPIO_Pin_4);
+	//SPI_NSSInternalSoftwareConfig(SPI1, SPI_NSSInternalSoft_Set);
+	return Data1;
 }
+
+void WriteReg(uint8_t Add , uint8_t value)
+{
+	GPIOB/*A*/->ODR &= ~GPIO_Pin_0/*4*/; // ReSet Pin CS//GPIO_ResetBits(GPIOA, GPIO_Pin_4);
+	_sendData(Add);
+	_sendData(value);
+	GPIOB/*A*/->ODR |= GPIO_Pin_0/*4*/; // Set Pin CS//GPIO_SetBits(GPIOA, GPIO_Pin_4);
+	delay_ms(10);
+}
+
+uint16_t ReadReg(uint8_t Add , uint16_t* value)
+{
+	uint16_t Data1 = 0;
+	GPIOB/*A*/->ODR &= ~GPIO_Pin_0/*4*/; // ReSet Pin CS//GPIO_ResetBits(GPIOA, GPIO_Pin_4);
+	_sendData(Add | READ_FLAG);
+	_sendData(0x00 | READ_FLAG);
+	Data1 = _readData(value);
+	GPIOB/*A*/->ODR |= GPIO_Pin_0/*4*/; // Set Pin CS//GPIO_SetBits(GPIOA, GPIO_Pin_4);
+	delay_ms(10);
+	return Data1;
+}
+/*-----------------------------------------------------------------------------------------------
+                                    INITIALIZATION
+usage: call this function at startup, giving the sample rate divider (raging from 0 to 255) and
+low pass filter value; suitable values are:
+BITS_DLPF_CFG_256HZ_NOLPF2
+BITS_DLPF_CFG_188HZ
+BITS_DLPF_CFG_98HZ
+BITS_DLPF_CFG_42HZ
+BITS_DLPF_CFG_20HZ
+BITS_DLPF_CFG_10HZ
+BITS_DLPF_CFG_5HZ
+BITS_DLPF_CFG_2100HZ_NOLPF
+returns 1 if an error occurred
+-----------------------------------------------------------------------------------------------*/
+#define MPU_InitRegNum 		21
+
+uint8_t MPU_Init(uint8_t LP_Filter)
+{
+    uint8_t i = 0;
+    uint8_t MPU_Init_Data[MPU_InitRegNum][2] =
+    {
+        {0x80, MPUREG_PWR_MGMT_1},     // Reset Device
+        {0x01, MPUREG_PWR_MGMT_1},     // Clock Source()
+        {0x00, MPUREG_PWR_MGMT_2},     // Enable Acc & Gyro
+
+        {LP_Filter, MPUREG_CONFIG},    // Use DLPF set Gyroscope bandwidth 184Hz, temperature bandwidth 188Hz
+        {0x18, MPUREG_GYRO_CONFIG},    // +-2000dps
+        {0x08, MPUREG_ACCEL_CONFIG},   // +-4G
+        {0x02, MPUREG_ACCEL_CONFIG_2}, // Set Acc Data Rates, Enable Acc LPF , Bandwidth 184Hz//changed,0-460,1-184,2-92,3-41,4-20,5-10,6-5,7-460
+//		{0x02, MPUREG_SMPLRT_DIV},
+
+        {0x30, MPUREG_INT_PIN_CFG},    //
+        //{0x40, MPUREG_I2C_MST_CTRL},   // I2C Speed 348 kHz
+        //{0x20, MPUREG_USER_CTRL},      // Enable AUX
+        {0x20, MPUREG_USER_CTRL},       // I2C Master mode
+        {0x0D, MPUREG_I2C_MST_CTRL}, //  I2C configuration multi-master  IIC 400KHz
+
+        {AK8963_I2C_ADDR, MPUREG_I2C_SLV0_ADDR},  //Set the I2C slave addres of AK8963 and set for write.
+        //{0x09, MPUREG_I2C_SLV4_CTRL},
+        //{0x81, MPUREG_I2C_MST_DELAY_CTRL}, //Enable I2C delay
+
+        {AK8963_CNTL2, MPUREG_I2C_SLV0_REG}, //I2C slave 0 register address from where to begin data transfer
+        {0x01, MPUREG_I2C_SLV0_DO}, // Reset AK8963
+        {0x81, MPUREG_I2C_SLV0_CTRL},  //Enable I2C and set 1 byte
+
+        {AK8963_CNTL1, MPUREG_I2C_SLV0_REG}, //I2C slave 0 register address from where to begin data transfer
+        {0x12, MPUREG_I2C_SLV0_DO}, // Register value to continuous measurement in 16bit
+        {0x81, MPUREG_I2C_SLV0_CTRL},  //Enable I2C and set 1 byte
+
+		{0x40, MPUREG_INT_ENABLE},//enable wake on interrupt
+        {0xC0,MPUREG_MOT_DETECT_CTRL},
+		{0x7F, MPUREG_WOM_THR},//Added to anable wake on thresold {0xff, MPUREG_WOM_THR},//Added to anable wake on thresold
+        {0x30, MPUREG_INT_PIN_CFG},//interrupt pin config pin high until interrupt clear
+    };
+
+    //spi.format(8,0); //consider
+    //spi.frequency(1000000); //consider
+
+    for(i=0; i<MPU_InitRegNum; i++)
+    {
+    	WriteReg(MPU_Init_Data[i][1], MPU_Init_Data[i][0]);
+    	//MPU_WriteReg(MPU_Init_Data[i][1], MPU_Init_Data[i][0]);
+    	//Spi_WriteReg(MPU_Init_Data[i][1], MPU_Init_Data[i][0]);
+        //Delay_ms(1);
+    	delay_ms(1);
+    	//wait(0.001);  //I2C must slow down the write speed, otherwise it won't work
+    }
+
+    //Set_Acc_Scale(2);
+    //Set_Gyro_Scale(250);
+
+    //AK8963_calib_Magnetometer();  //Can't load this function here , strange problem?
+    return 0;
+}
+
 
 /*	GPIO Toggle*/
 int main(void)
@@ -328,7 +429,7 @@ printf("Starting....!!\r\n");
 
 char buffer[80] = {'\0'};
 uint32_t RTC_Counter = 0;
-uint8_t i;
+uint16_t i;
 
 RTC_DateTimeTypeDef	RTC_DateTime;
 GPIO_InitTypeDef  GPIO_InitStructure;
@@ -345,79 +446,92 @@ USB_Init();*/
 
 /* Initialize LED which connected to PC13 	 /
 /  Enable PORTC Clock						*/
-RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-/* Configure the GPIO_LED pin */
-GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
-GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-GPIO_Init(GPIOC, &GPIO_InitStructure);
-
-//GPIO_SetBits(GPIOC, GPIO_Pin_13); // Set C13 to High level ("1")
-GPIO_ResetBits(GPIOC, GPIO_Pin_13); // Set C13 to Low level ("0")
+//RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+///* Configure the GPIO_LED pin */
+//GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+//GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+//GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
+//GPIO_Init(GPIOC, &GPIO_InitStructure);
+//
+////GPIO_SetBits(GPIOC, GPIO_Pin_13); // Set C13 to High level ("1")
+//GPIO_ResetBits(GPIOC, GPIO_Pin_13); // Set C13 to Low level ("0")
 
 RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
 RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 
 GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
 GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
-GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7/*MOSI1*/ |  GPIO_Pin_5;/*SCLK*/
+GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7/*MOSI1*/ | GPIO_Pin_5 /*SCLK1*/;// | GPIO_Pin_6 /*MISO1*/;
 GPIO_Init(GPIOA, &GPIO_InitStructure);
 
 GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
 GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
-GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_4;/*MISO1*/
+GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;/*MISO*/
 GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;//4;/*CS*/
+GPIO_Init(GPIOB/*A*/, &GPIO_InitStructure);
 //GPIO_ResetBits(GPIOA, GPIO_Pin_6);
-//GPIO_SetBits(GPIOA, GPIO_Pin_6);
+GPIO_SetBits(GPIOB/*A*/, GPIO_Pin_0/*4*/);
 
 SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
 //SPI_InitStructure.SPI_Direction = SPI_Direction_1Line_Tx;
 SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
 SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
 SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
-SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
-SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_8; /*(Fpclk/8 = 8Mhz/8 = 1Mhz)*/
+SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;// | SPI_NSSInternalSoft_Set;
+SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_4; /*(Fpclk/8 = 8Mhz/8 = 1Mhz)*/
 SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
 SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
 SPI_Init(SPI1, &SPI_InitStructure);
 SPI_Cmd(SPI1, ENABLE);
-SPI_NSSInternalSoftwareConfig(SPI1, SPI_NSSInternalSoft_Set);
+//SPI_NSSInternalSoftwareConfig(SPI1, SPI_NSSInternalSoft_Set);
 
-if (RTC_Init() == 1)
-{
-	// ßêùî ïåðøà ³í³ö³àë³çàö³ÿ RTC Âñòàíîâëþºìî ïî÷àòêîâó äàòó, íàïðèêëàä 22.09.2016 14:30:00
-	RTC_DateTime.RTC_Date = 27;
-	RTC_DateTime.RTC_Month = 2;
-	RTC_DateTime.RTC_Year = 2018;
 
-	RTC_DateTime.RTC_Hours = 19;
-	RTC_DateTime.RTC_Minutes = 53;
-	RTC_DateTime.RTC_Seconds = 00;
+//if (RTC_Init() == 1)
+//{
+//	// ßêùî ïåðøà ³í³ö³àë³çàö³ÿ RTC Âñòàíîâëþºìî ïî÷àòêîâó äàòó, íàïðèêëàä 22.09.2016 14:30:00
+//	RTC_DateTime.RTC_Date = 27;
+//	RTC_DateTime.RTC_Month = 2;
+//	RTC_DateTime.RTC_Year = 2018;
+//
+//	RTC_DateTime.RTC_Hours = 19;
+//	RTC_DateTime.RTC_Minutes = 53;
+//	RTC_DateTime.RTC_Seconds = 00;
+//
+//	// Ï³ñëÿ ³í³ö³àë³çàö³¿ ïîòð³áíà çàòðèìêà. Áåç íå¿ ÷àñ íå âñòàíîâëþºòüñÿ.
+//	delay_ms(500);
+//	RTC_SetCounter(RTC_GetRTC_Counter(&RTC_DateTime));
+//}
+//GPIO_SetBits(GPIOA, GPIO_Pin_4);
+GPIOB/*A*/->ODR |= GPIO_Pin_0; // Set Pin CS
 
-	// Ï³ñëÿ ³í³ö³àë³çàö³¿ ïîòð³áíà çàòðèìêà. Áåç íå¿ ÷àñ íå âñòàíîâëþºòüñÿ.
-	delay_ms(500);
-	RTC_SetCounter(RTC_GetRTC_Counter(&RTC_DateTime));
-}
+MPU_Init(BITS_DLPF_CFG_20HZ);
 
 while(1)
 {
-	GPIOC->ODR ^= GPIO_Pin_13; // Invert C13
-	RTC_Counter = RTC_GetCounter();
-
-	RTC_GetDateTime(RTC_Counter, &RTC_DateTime);
-	printf("%02d.%02d.%04d  %02d:%02d:%02d\n",
-			RTC_DateTime.RTC_Date, RTC_DateTime.RTC_Month, RTC_DateTime.RTC_Year,
-			RTC_DateTime.RTC_Hours, RTC_DateTime.RTC_Minutes, RTC_DateTime.RTC_Seconds);
-
-	// Ôóíêö³ÿ ãåíåðóº ó áóôåð³ äàòó âëàñíîãî ôîðìàòó
-	RTC_GetMyFormat(&RTC_DateTime, buffer);
-
-	/* delay */
-	while (RTC_Counter == RTC_GetCounter())
-	{
-	}
-	_readData(0x75,&i);
-	printf("I_am = %d\n\r",i);
+//	GPIOC->ODR ^= GPIO_Pin_13; // Invert C13
+//	RTC_Counter = RTC_GetCounter();
+//
+//	RTC_GetDateTime(RTC_Counter, &RTC_DateTime);
+//	printf("%02d.%02d.%04d  %02d:%02d:%02d\n",
+//			RTC_DateTime.RTC_Date, RTC_DateTime.RTC_Month, RTC_DateTime.RTC_Year,
+//			RTC_DateTime.RTC_Hours, RTC_DateTime.RTC_Minutes, RTC_DateTime.RTC_Seconds);
+//
+//	// Ôóíêö³ÿ ãåíåðóº ó áóôåð³ äàòó âëàñíîãî ôîðìàòó
+//	RTC_GetMyFormat(&RTC_DateTime, buffer);
+//
+//	/* delay */
+//	while (RTC_Counter == RTC_GetCounter())
+//	{
+//	}
+	i = ReadReg(MPUREG_WHOAMI, 0);
+//	_sendData(0x75 | 0x80);
+//	_readData(&i);
+	printf("I_am = %u\n\r",i);
+	delay_ms(10);
 }
 
 /*while (1)
